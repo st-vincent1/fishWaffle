bcfff9fefd4bf57d57e763514b36bb4e2ca71a94
xiazczyk <bastianksiazczyk@gmail.com>
2020-01-16 13:46:40 +0000

    1 epoch: checking if plots work

--- src/modeling.py
+++ src/modeling.py
@@ -93,8 +93,10 @@ def create_model(predictors, label, max_sequence_len, total_words):
 	model.compile(loss='categorical_crossentropy', optimizer=Adam(lr = 2e-3), metrics=['accuracy'])
 	# earlystop = EarlyStopping(monitor='val_loss', min_delta=1, patience=5, verbose=0, mode='auto')
 	h = model.fit(predictors, label, epochs=1, verbose=1, batch_size=512)
-	plt.bar(h.history['loss'])
-	plt.show()
+	print(h.history['loss'])
+	print(h.history['accuracy'])
+	# plt.bar(h.history['loss'])
+	# plt.show()
 	print(model.summary())
 	return model
 
