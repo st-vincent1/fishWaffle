1695a537fb43fa72ff01079227a90f6d709f5e3b
xiazczyk <bastianksiazczyk@gmail.com>
2020-01-22 14:56:40 +0000

    epochs -> 1

--- src/modeling.py
+++ src/modeling.py
@@ -106,7 +106,7 @@ def create_model(predictors, label, max_sequence_len, total_words):
 	# earlystop = EarlyStopping(monitor='val_loss', min_delta=1, patience=5, verbose=0, mode='auto')
 	h = model.fit(predictors,
 				  label,
-				  epochs=250,
+				  epochs=1,
 				  verbose=1,
 				  batch_size=1024,
 				  validation_split = 0.2)
--- src/train_script.sh
+++ src/train_script.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-python modeling.py origin 2>&1 | tee -a originerr.txt
-python modeling.py speakers 2>&1 | tee -a speakerserr.txt
+python train_waffler.py origin 2>&1 | tee -a /home/acq19sk/originerr.txt
+python train_waffler.py speakers 2>&1 | tee -a /home/acq19sk/speakerserr.txt
