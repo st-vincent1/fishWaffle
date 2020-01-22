fc24e0c7e022f5459486988741e0d8d5816021f7
xiazczyk <bastianksiazczyk@gmail.com>
2020-01-22 11:31:57 +0000

    training on 250 again

--- src/modeling.py
+++ src/modeling.py
@@ -101,7 +101,7 @@ def create_model(predictors, label, max_sequence_len, total_words):
 
 	model.compile(loss='categorical_crossentropy', optimizer=Adam(lr = 2e-3), metrics=['accuracy'])
 	# earlystop = EarlyStopping(monitor='val_loss', min_delta=1, patience=5, verbose=0, mode='auto')
-	h = model.fit(predictors, label, epochs=1, verbose=1, batch_size=512)
+	h = model.fit(predictors, label, epochs=250, verbose=1, batch_size=1024)
 	print(model.summary())
 	# summarize history for accuracy
 	fig = plt.figure()
